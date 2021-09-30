88a484df5efcf6794c37f9488db27dc4ba0e72c2
uvsq22107773 <abd-elmoumen.drici@ens.uvsq.fr>
2021-09-30 13:45:32 +0200

    adding le constructeur et la methode toString

---
+++ Fraction.java
@@ -0,0 +1,14 @@
+public class Fraction {
+    Fraction(){
+
+    }
+    public String toString(String ss){
+        String s = ss ;
+        return s ;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello!!");
+    }
+}
